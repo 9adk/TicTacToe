39a591b1eeb421b305c0067df0e426708c98081e
aditya Kharade <adityakharade51@gmail.com>
2020-10-08 09:55:57 +0530

    Added : added a functionality to give symbol

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
 
 public class TicTacToeGame {
-	public static char[] tictactoe = new char[10];
-	static String s = " ";
-	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	public static String[] tictactoe = new String[10];
+	String computer = "";
+	public void createBoard() {
 		for(int i = 1; i < tictactoe.length; i++ ){
-				tictactoe[i] = s.charAt(0);
-				System.out.println(i+" is "+tictactoe[i]);
-			}
+			tictactoe[i] = " ";
+		}
 	}
+	public void computerSymbol() {
+		String player;
+		do {	
+			System.out.println("Enter the symbol for player from X or O");
+			player = scanner.nextLine();
+			if(player.equals("X")) {
+				computer = "O";
+				break;
+			}
+			else if(player.equals("O")){
+				computer = "X";
+				break;
+			}
+			else {
+				System.out.println("Invalid input");
+			}
+		}while(player!="O" || player!="X");
+	}
+	public static void main(String[] args) {
+		TicTacToeGame t = new TicTacToeGame();
+		t.createBoard();
+		t.computerSymbol();	
+		}
 }
